5911fcb2b7057af0de132d362f53f2ee7a18ecad
MaheshMERN <88592368+MaheshMERN@users.noreply.github.com>
2023-09-16 20:50:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function calculateMinCost() {
-  //your code here
   
-  
-  
+	 const inputValue = document.getElementById("rope-lengths").value;
+
+            // Split the input value into an array using a comma as the delimiter
+            const inputArray = inputValue.split(',');
+            ropes = inputArray.map(Number)
+            // console.log(ropes)
+} 
+let output;
+let result = function minCostOfRopes(ropes) {
+  if (ropes.length < 2) {
+    return 0; // No ropes or only one rope, no cost to connect.
+  }
+
+  ropes.sort((a, b) => a - b);
+
+  let totalCost = 0;
+  while (ropes.length > 1) {
+    const firstRope = ropes.shift();
+    const secondRope = ropes.shift();
+    const cost = firstRope + secondRope;
+    totalCost += cost;
+    ropes.push(cost);
+    ropes.sort((a, b) => a - b);
+  }
+// console.log(totalCost)
+	output = totalCost;
+  return totalCost;
+}
+ result(ropes)
+document.getElementById("result").innerHTML = output;
 }  
